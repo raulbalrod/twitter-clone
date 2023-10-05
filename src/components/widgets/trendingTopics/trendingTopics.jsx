4514e1de2a3103395018d28fb47c66e47966a974
raulbalrod <119789823+raulbalrod@users.noreply.github.com>
2023-10-05 16:42:27 +0200

    beginning of the components of the widgets section

---
+++ src/components/widgets/trendingTopics/trendingTopics.jsx
@@ -0,0 +1,15 @@
+export const TredingTopics = () => {
+  return (
+    <div className='widgets'>
+      <div className='feed-header'>
+        <h2>Widgets</h2>
+
+        {/* Suscribe to Premium */}
+
+        {/* Who to follow */}
+
+        {/* Trending topics */}
+      </div>
+    </div>
+  );
+};
--- src/components/widgets/widgets.jsx
+++ src/components/widgets/widgets.jsx
@@ -1,9 +1,27 @@
+import { Button } from '@mui/material';
+import './Widgets.css';
+
+import { SubscriptionsTwitter } from './subscription/subscription';
+import { TredingTopics } from './trendingTopics/trendingTopics';
+import { WhoToFollows } from './whoToFollow/whoToFollow';
+
 export const WidgetsTwitter = () => {
   return (
     <div className='widgets'>
-      <div className='feed-header'>
-        <h2>Widgets</h2>
+      <div className='subscription-premium'>
+        <h3>Suscribe to Premium</h3>
+        <span>
+          Subscribe to unlock new features and if eligible, receive a share of
+          ads revenue.
+        </span>
+        <Button className='subscribe-premium-btn'>Subscribe</Button>
       </div>
+
+      <SubscriptionsTwitter />
+
+      <WhoToFollows />
+
+      <TredingTopics />
     </div>
   );
 };
